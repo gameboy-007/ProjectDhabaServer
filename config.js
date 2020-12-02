abc5f5d4260e5e7c8471ce908718a0e04c988f80
DEEPESH001 <32238571+DEEPESH001@users.noreply.github.com>
2020-12-02 21:25:19 +0530

    Server completed

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '202395624674689',
+        clientSecret: '797037eb05537cfbc6455bdb71f834b3'
+    }
+}
